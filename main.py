965193581c9892453ec57b7bb185887bf45b7369
PyDevIll <delphadyth@gmail.com>
2024-06-07 16:33:16 +0000

    Added handling empty or filtered out lists

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ def process_numbers(numbers):
     is_even = lambda x: x%2 == 0
     square = lambda x: x*x
     accumulate = lambda a, b: a+b
-
+    
+    if len(numbers) == 0:
+        return 0
+        
     numbers = filter(is_positive, numbers)
     numbers = filter(is_even, numbers)
     numbers = map(square, numbers)
-    numbers = reduce(accumulate, numbers)
+    numbers = reduce(accumulate, numbers, 0)
     
     return numbers
     
