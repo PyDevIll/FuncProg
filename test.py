3cbee0945e6cac8afea49c98d2675642ac81ab9d
PyDevIll <delphadyth@gmail.com>
2024-06-07 16:33:56 +0000

    More test cases

--- test.py
+++ test.py
@@ -3,4 +3,8 @@ from main import process_numbers
 
 def test_process_numbers():
     assert process_numbers([1, 2, 3, 4, 5, -2, -4]) == 20
-
+    assert process_numbers([]) == 0
+    assert process_numbers([0]) == 0
+    assert process_numbers([1]) == 0
+    assert process_numbers([1, 3, 5, 7]) == 0
+    assert process_numbers([-2, -4, -6, -8]) == 0
